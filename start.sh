6d0c1a6c191e24a6c3d8b6bca49542ab1fe9332f
avingtier <avingtier@gmail.com>
2020-12-12 17:09:48 +0100

    minor fix

--- start.sh
+++ start.sh
@@ -1,21 +1,21 @@
-printf "\n\033[31m--------------------------------------------------------------------------\033[0m"
-printf "\n\033[31m Stopping and clenaning docker environment \033[0m"
-printf "\n\033[31m--------------------------------------------------------------------------\033[0m"
+printf "\n\033[32m--------------------------------------------------------------------------\033[0m"
+printf "\n\033[32m Stopping and clenaning docker environment \033[0m"
+printf "\n\033[32m--------------------------------------------------------------------------\033[0m"
 printf "\n"
 docker swarm leave --force
 docker system prune --all --force
 
 
-printf "\n\033[31m--------------------------------------------------------------------------\033[0m"
-printf "\n\033[31m Initializing docker environment \033[0m"
-printf "\n\033[31m--------------------------------------------------------------------------\033[0m"
+printf "\n\033[32m--------------------------------------------------------------------------\033[0m"
+printf "\n\033[32m Initializing docker environment \033[0m"
+printf "\n\033[32m--------------------------------------------------------------------------\033[0m"
 printf "\n"
 docker swarm init
 docker network create --driver=overlay --attachable shared-proxy
 
-printf "\n\033[31m--------------------------------------------------------------------------\033[0m"
-printf "\n\033[31m Run portainer \033[0m"
-printf "\n\033[31m--------------------------------------------------------------------------\033[0m"
+printf "\n\033[32m--------------------------------------------------------------------------\033[0m"
+printf "\n\033[32m Run portainer \033[0m"
+printf "\n\033[32m--------------------------------------------------------------------------\033[0m"
 printf "\n"
 docker stack deploy portainer --compose-file docker-compose.yml
-echo "\n You can access to portainer here : http://418.ovh:9000\n"
+printf "\n You can access to portainer here : http://418.ovh:9000\n"
