b5d1ec1bbff218f0f4bc37cf9a3002036e56d408
avingtier <avingtier@gmail.com>
2020-12-12 17:13:27 +0100

    simplify initialization script

--- reset.sh
+++ reset.sh
@@ -4,7 +4,7 @@ printf "\n\033[32m--------------------------------------------------------------
 printf "\n"
 docker swarm leave --force
 docker system prune --all --force
-
+rm -rf /var/portainer/data/*
 
 printf "\n\033[32m--------------------------------------------------------------------------\033[0m"
 printf "\n\033[32m Initializing docker environment \033[0m"
