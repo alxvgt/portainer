c86ce4379d16e496dba07579533a2cf7821277da
avingtier <avingtier@gmail.com>
2020-12-18 18:17:30 +0100

    update restart script

--- restart.sh
+++ restart.sh
@@ -1,14 +1,13 @@
 printf "\n\033[32m--------------------------------------------------------------------------\033[0m"
-printf "\n\033[32m Stopping and clenaning docker environment \033[0m"
+printf "\n\033[32m Remove stack portainer \033[0m"
 printf "\n\033[32m--------------------------------------------------------------------------\033[0m"
 printf "\n"
-docker swarm leave --force
-docker system prune --all --force
-rm -rf /var/portainer/data/*
+docker stack rm portainer
 
+printf "\n You can access to portainer here : http://418.ovh:9000\n"
 printf "\n\033[32m--------------------------------------------------------------------------\033[0m"
-printf "\n\033[32m Initializing docker environment \033[0m"
+printf "\n\033[32m Deploy stack portainer \033[0m"
 printf "\n\033[32m--------------------------------------------------------------------------\033[0m"
 printf "\n"
-docker swarm init
-docker network create --driver=overlay --attachable shared-proxy
+docker stack deploy portainer --compose-file docker-compose.yml
+printf "\n\n You can access to portainer here : http://418.ovh:9000 or https://portainer.418.ovh\n"
--- start.sh
+++
@@ -1,6 +0,0 @@
-printf "\n\033[32m--------------------------------------------------------------------------\033[0m"
-printf "\n\033[32m Run portainer \033[0m"
-printf "\n\033[32m--------------------------------------------------------------------------\033[0m"
-printf "\n"
-docker stack deploy portainer --compose-file docker-compose.yml
-printf "\n You can access to portainer here : http://418.ovh:9000\n"
